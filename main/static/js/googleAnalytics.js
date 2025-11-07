40674ea430c54c7c2fcebef4bbb9ac3a99bae27c
Blessing-Mbalaka <bjmbalaka@gmail.com>
2025-11-07 02:14:31 +0200

    Small provision for google Analytics---Real configuration to be done once domain name is set up

---
+++ main/static/js/googleAnalytics.js
@@ -0,0 +1,12 @@
+// Replace 'YOUR_MEASUREMENT_ID' with your actual Google Analytics Measurement ID
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'YOUR_MEASUREMENT_ID');
+
+
+
+
+
