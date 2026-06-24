e72f4341d0761ec9d61f82a944f4aa2186d09cc2
Blessing Mbalaka <132813484+Blessing-Mbalaka@users.noreply.github.com>
2026-06-25 01:13:43 +0200

    Update settings.py

--- stem_app/settings.py
+++ stem_app/settings.py
@@ -47,7 +47,8 @@ SECRET_KEY = 'django-insecure-!zn*13(debh3r_#^)dki$0ht&y22#sjt@b@c&4y+y0rv&tg9yn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,testserver").split(",")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,testserver,162.35.167.180").split(",")
 
 
 # Application definition
